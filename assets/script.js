bf967bdff5a5161212062de87e7544ae63d5ded3
Kacie Hatley <kaciehatley@gmail.com>
2019-11-20 16:29:32 -0500

    current weather data displays when city is searched

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,14 @@ var searchHistory = document.querySelector(".searchHistory");
 var searchIcon = document.querySelector(".search-icon");
 var inputBox = document.querySelector(".searchInput");
 var currentWeatherBox = document.querySelector(".currentWeather");
-currentWeatherBox.innerHTML = "Hello"
+
 var userHistory = [];
 
 function renderWeather() {
     searchHistory.innerHTML = "";
     var cityValue = inputBox.value;
     userHistory.push(cityValue);
+    currentWeatherBox.innerHTML="";
     
     for (var i=0; i < userHistory.length; i++) {
         var historyBlock = document.createElement("div");
@@ -23,6 +24,32 @@ function renderWeather() {
         url: weatherURL,
         method: "GET"
     }).then(function(response) {
+        var cityName = document.createElement("h1");
+        var weatherIcon = document.createElement("img");
+        var weatherIconHTML = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        var temp = document.createElement("p");
+        var humidity = document.createElement("p");
+        var wind = document.createElement("p");
+
+
+        cityName.innerHTML = response.name;
+        temp.innerHTML = "Temperature: " + response.main.temp;
+        humidity.innerHTML = "Temperature: " + response.main.humidity;
+        humidity.innerHTML = "Humidity: " + response.main.humidity + "%";
+        wind.innerHTML = "Wind Speed: " + response.wind.speed + " MPH";
+
+        cityName.setAttribute("class", "cityName");
+        weatherIcon.setAttribute("src", weatherIconHTML);
+        weatherIcon.setAttribute("class", "weatherIcon");
+
+        currentWeatherBox.appendChild(cityName);
+        currentWeatherBox.appendChild(weatherIcon);
+        currentWeatherBox.appendChild(temp);
+        currentWeatherBox.appendChild(humidity);
+        currentWeatherBox.appendChild(wind);
+        
+
+
         console.log(response.name);
         console.log(response.main.temp);
         console.log(response.main.humidity);
