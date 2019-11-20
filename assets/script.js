7fe6c525a9e6b6f5cf0e8fd98734c36c3e4c12d2
Kacie Hatley <kaciehatley@gmail.com>
2019-11-20 13:37:28 -0500

    current weather api added and working

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
 var searchHistory = document.querySelector(".searchHistory");
 var searchIcon = document.querySelector(".search-icon");
 var inputBox = document.querySelector(".searchInput");
+var currentWeatherBox = document.querySelector(".currentWeather");
+currentWeatherBox.innerHTML = "Hello"
 var userHistory = [];
 
-function createButtons() {
+function renderWeather() {
     searchHistory.innerHTML = "";
-    var inputValue = inputBox.value;
-    userHistory.push(inputValue);
+    var cityValue = inputBox.value;
+    userHistory.push(cityValue);
     
     for (var i=0; i < userHistory.length; i++) {
         var historyBlock = document.createElement("div");
@@ -14,9 +16,22 @@ function createButtons() {
         historyBlock.innerHTML = userHistory[i];
         searchHistory.prepend(historyBlock);
     }
+
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+ inputBox.value + "&appid=8f851cd1ee25a4bb8996f7bb698edf09";
+
+    $.ajax({
+        url: weatherURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response.name);
+        console.log(response.main.temp);
+        console.log(response.main.humidity);
+        console.log(response.wind.speed);
+    });
 }
 
-searchIcon.addEventListener("click", createButtons);
+searchIcon.addEventListener("click", renderWeather);
+
 
 
 
